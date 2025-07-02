32f1b79c59d33977d8b1656ca6674d1062e92240
MarinaMiron <marina.miron1309@gmail.com>
2025-07-02 14:59:20 +0300

    Checking for collisions

--- include/Game.h
+++ include/Game.h
@@ -12,6 +12,7 @@ private:
     bool IsBlockOutside();
     void RotateBlock();
     void LockBlock();
+    bool BlockFits();
 public:
     Game();
     void Draw();
--- src/Game.cpp
+++ src/Game.cpp
@@ -49,7 +49,7 @@ bool Game::IsBlockOutside()
 void Game::RotateBlock()
 {
     currentBlock.Rotate();
-    if(IsBlockOutside())
+    if(IsBlockOutside() || BlockFits() == false)
     {
         currentBlock.UndoRotation();
     }
@@ -89,10 +89,23 @@ void Game::LockBlock()
     nextBlock = GetRandomBlock();
 }
 
+bool Game::BlockFits()
+{
+    std::vector<Position> tiles = currentBlock.GetCellPositions();
+    for(Position item: tiles)
+    {
+        if(grid.IsCellEmpty(item.row, item.column) == false)
+        {
+            return false;
+        }
+    }
+    return true;
+}
+
 void Game::MoveBlockLeft()
 {
     currentBlock.Move(0, -1);
-    if (IsBlockOutside())
+    if (IsBlockOutside() || BlockFits() == false)
     {
         currentBlock.Move(0, 1);
     }
@@ -101,7 +114,7 @@ void Game::MoveBlockLeft()
 void Game::MoveBlockRight()
 {
     currentBlock.Move(0, 1);
-    if (IsBlockOutside())
+    if (IsBlockOutside() || BlockFits() == false)
     {
         currentBlock.Move(0, -1);
     }
@@ -110,7 +123,7 @@ void Game::MoveBlockRight()
 void Game::MoveBlockDown()
 {
     currentBlock.Move(1, 0);
-    if (IsBlockOutside())
+    if (IsBlockOutside() || BlockFits() == false)
     {
         currentBlock.Move(-1, 0);
         LockBlock();
